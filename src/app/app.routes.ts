eac57206c45132177ecf48c18b9b7d05ffde4480
Thirumoorthy Ponnusamy <thirumoorthy.ponnusamy@trustrace.com>
2023-07-26 17:49:38 +0530

    list and manage page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./home/home.component'),
   },
+  {
+    path: 'manage',
+    loadComponent: () => import('./manage/manage.component'),
+  },
+  {
+    path: 'list',
+    loadComponent: () => import('./list/list.component'),
+  },
 ];
---
+++ src/app/manage/manage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-manage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss'],
+})
+export default class ManageComponent {}
