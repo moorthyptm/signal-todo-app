351dd753381ee2cabcfbcca932da910e52a98038
Thirumoorthy Ponnusamy <thirumoorthy.ponnusamy@trustrace.com>
2023-07-26 14:48:36 +0530

    header component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'signal-todo-app';
